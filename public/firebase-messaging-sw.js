ba04c4edb2a4376d58200fdf07c30cbe08593783
s414june <s414june@gmail.com>
2023-12-15 20:10:43 +0800

    update

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+// Firebase v9+ 導入方式
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDc7_KRggZySk2NLdlcEQGfAtOuyVC-REs",
+	authDomain: "pwa-notification-test-39b3f.firebaseapp.com",
+	projectId: "pwa-notification-test-39b3f",
+	storageBucket: "pwa-notification-test-39b3f.appspot.com",
+	messagingSenderId: "667279105190",
+	appId: "1:667279105190:web:b653b12ae2b15b4db3a867",
+	measurementId: "G-28EW5Q8N5D",
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  const title = payload.notification.title;
+  const options = {
+    body: payload.notification.body,
+    icon: payload.notification.icon
+  };
+
+  return self.registration.showNotification(title, options);
+});
