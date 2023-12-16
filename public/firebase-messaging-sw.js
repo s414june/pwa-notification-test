5958858912b8bebc1411745c1c55e8ea24468a66
s414june <s414june@gmail.com>
2023-12-16 14:55:47 +0800

    u

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -38,7 +38,7 @@ messaging.onBackgroundMessage(function (payload) {
 
 // 註冊推播通知事件處理程序
 self.addEventListener("notificationclick", function (event) {
-  console.log(event)
+  console.log(event);
   event.notification.close(); // 關閉通知
   // 在這裡添加您希望的點擊通知後執行的代碼
   // 例如，打開特定頁面或執行其他操作
@@ -48,15 +48,19 @@ self.addEventListener("notificationclick", function (event) {
 });
 
 self.addEventListener("message", (event) => {
-  console.log(event.data)
+  console.log(event.data);
   if (event.data.action === "skipWaiting") {
     self.skipWaiting();
   }
 });
 
-self.addEventListener("push", () => {
+self.addEventListener("push", (event) => {
   // clearCache(event)
   // self.skipWaiting();
+  console.log(event.data);
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener("activate", (event) => {
